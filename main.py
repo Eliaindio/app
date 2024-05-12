13bacd3638dff049968559850e10022d97676e0f
Eliaquim Souza <eliaquim@alunos.utfpr.edu.br>
2024-05-12 19:46:33 -0300

    Final_Version

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 import threading
 
 class TarefaProcesso:
-    def __init__(self, pid, ppid, memoria, cpu, leitura, escrita):
+    def __init__(self, nome, pid, ppid, memoria, cpu, leitura, escrita):
+        self.nome = nome
         self.pid = pid
         self.ppid = ppid
         self.memoria = memoria
@@ -16,7 +17,8 @@ class TarefaProcesso:
 
 
 class TarefaMemoria:
-    def __init__(self, pid, total_memory, code_memory, heap_memory, stack_memory, total_pages, code_pages, heap_pages, stack_pages):
+    def __init__(self, nome, pid, total_memory, code_memory, heap_memory, stack_memory, total_pages, code_pages, heap_pages, stack_pages):
+        self.nome = nome
         self.pid = pid
         self.total_memory = total_memory
         self.code_memory = code_memory
@@ -53,7 +55,8 @@ class GerenciadorTarefas:
 
  
         # Criar tabela_processos de tarefas
-        self.tabela_processos = ttk.Treeview(self.tab_processos, columns=("PID", "PPID", "Memória", "CPU", "Leitura", "Escrita"))
+        self.tabela_processos = ttk.Treeview(self.tab_processos, columns=("Nome","PID", "PPID", "Memória", "CPU", "Leitura", "Escrita"))
+        self.tabela_processos.heading("Nome", text="Nome")
         self.tabela_processos.heading("PID", text="PID")
         self.tabela_processos.heading("PPID", text="PPID")
         self.tabela_processos.heading("Memória", text="Memória")
@@ -62,27 +65,28 @@ class GerenciadorTarefas:
         self.tabela_processos.heading("Escrita", text="Escrita")
         self.tabela_processos.pack(expand=True, fill=tk.BOTH, side="top")
         
-        self.tabela_memoria = ttk.Treeview(self.tab_memoria, columns=("PID", "Memoria Total", "Memoria Codigo", "Memoria Heap", "Memoria Stack",
+        self.tabela_memoria = ttk.Treeview(self.tab_memoria, columns=("Nome", "PID", "Memoria Total", "Memoria Codigo", "Memoria Heap", "Memoria Stack",
                                                                  "Total de Paginas", "Paginas Codigos", "Paginas Heap", "Paginas Stack"))
+        self.tabela_memoria.heading("Nome", text="Nome")
         self.tabela_memoria.heading("PID", text="PID")
         self.tabela_memoria.heading("Memoria Total", text="Memoria Total")
-        self.tabela_memoria.heading("Memoria Codigo", text="code_memory")
-        self.tabela_memoria.heading("Memoria Heap", text="heap_memory")
-        self.tabela_memoria.heading("Memoria Stack", text="stack_memory")
-        self.tabela_memoria.heading("Total de Paginas", text="total_pages")
-        self.tabela_memoria.heading("Paginas Codigos", text="code_pages")
-        self.tabela_memoria.heading("Paginas Heap", text="heap_pages")
-        self.tabela_memoria.heading("Paginas Stack", text="stack_pages")
+        self.tabela_memoria.heading("Memoria Codigo", text="Memoria Codigo")
+        self.tabela_memoria.heading("Memoria Heap", text="Memoria Heap")
+        self.tabela_memoria.heading("Memoria Stack", text="Memoria Stack")
+        self.tabela_memoria.heading("Total de Paginas", text="Total de Paginas")
+        self.tabela_memoria.heading("Paginas Codigos", text="Paginas Codigos")
+        self.tabela_memoria.heading("Paginas Heap", text="Paginas Heap")
+        self.tabela_memoria.heading("Paginas Stack", text="Paginas Stack")
         self.tabela_memoria.pack(expand=True, fill=tk.BOTH, side="top")
         
         self.tabela_global = ttk.Treeview(self.tab_global, columns=("RAM Total", "RAM Livre", "Porcentagem de RAM utilizada", "Swap Total", 
                                                                 "Swap Livre", "Porcentagem de Swap utilizado"))
-        self.tabela_global.heading("RAM Total", text="total_ram")
-        self.tabela_global.heading("RAM Livre", text="free_ram")
-        self.tabela_global.heading("Porcentagem de RAM utilizada", text="ram_usage_percentage")
-        self.tabela_global.heading("Swap Total", text="total_swap")
-        self.tabela_global.heading("Swap Livre", text="free_swap")
-        self.tabela_global.heading("Porcentagem de Swap utilizado", text="swap_usage_percentage")
+        self.tabela_global.heading("RAM Total", text="RAM Total")
+        self.tabela_global.heading("RAM Livre", text="RAM Livre")
+        self.tabela_global.heading("Porcentagem de RAM utilizada", text="Porcentagem de RAM utilizada")
+        self.tabela_global.heading("Swap Total", text="Swap Total")
+        self.tabela_global.heading("Swap Livre", text="Swap Livre")
+        self.tabela_global.heading("Porcentagem de Swap utilizado", text="Porcentagem de Swap utilizado")
         self.tabela_global.pack(expand=True, fill=tk.BOTH, side="top")
 
 
@@ -113,6 +117,7 @@ class GerenciadorTarefas:
 
         tarefas_processos = []
         for tarefa in dados:
+            nome = tarefa["name"]
             pid = tarefa["pid"]
             ppid = tarefa["ppid"]
             memoria = str(tarefa["mem_usage_mb"]) + " Mb"
@@ -120,7 +125,7 @@ class GerenciadorTarefas:
             leitura = str(tarefa["total_read_bytes"]) + " b"
             escrita = str(tarefa["total_write_bytes"]) + " b"
 
-            nova_tarefa = TarefaProcesso(pid, ppid, memoria, cpu, leitura, escrita)
+            nova_tarefa = TarefaProcesso(nome, pid, ppid, memoria, cpu, leitura, escrita)
             tarefas_processos.append(nova_tarefa)
         
         self.limpar_tabela_processos()
@@ -132,6 +137,7 @@ class GerenciadorTarefas:
             dados = json.load(arquivo)
         tarefas_memoria = []
         for tarefa in dados:
+            nome = tarefa["name"]
             pid = tarefa["pid"]
             total_memory = tarefa["total_memory"]
             code_memory = tarefa["code_memory"]
@@ -141,7 +147,7 @@ class GerenciadorTarefas:
             code_pages = tarefa["code_pages"]
             heap_pages = tarefa["heap_pages"]
             stack_pages = tarefa["stack_pages"]
-            nova_tarefa = TarefaMemoria(pid, total_memory, code_memory, heap_memory, stack_memory, total_pages, code_pages, heap_pages, stack_pages)
+            nova_tarefa = TarefaMemoria(nome, pid, total_memory, code_memory, heap_memory, stack_memory, total_pages, code_pages, heap_pages, stack_pages)
             tarefas_memoria.append(nova_tarefa)
 
 
@@ -169,11 +175,11 @@ class GerenciadorTarefas:
     
 
     def inserir_tarefa_processo(self, tarefa):
-        valores = (tarefa.pid, tarefa.ppid, tarefa.memoria, tarefa.cpu, tarefa.leitura, tarefa.escrita)
+        valores = (tarefa.nome, tarefa.pid, tarefa.ppid, tarefa.memoria, tarefa.cpu, tarefa.leitura, tarefa.escrita)
         self.tabela_processos.insert("", tk.END, values=valores)
     
     def inserir_tarefa_memoria(self, tarefa):
-        valores = (tarefa.pid, tarefa.total_memory, tarefa.code_memory, tarefa.heap_memory, tarefa.total_pages, tarefa.code_pages, tarefa.heap_pages, tarefa.stack_pages)
+        valores = (tarefa.nome, tarefa.pid, tarefa.total_memory, tarefa.code_memory, tarefa.heap_memory, tarefa.total_pages, tarefa.code_pages, tarefa.heap_pages, tarefa.stack_pages)
         self.tabela_memoria.insert("", tk.END, values=valores)
 
     def inserir_tarefa_global(self, tarefa):
@@ -194,7 +200,7 @@ class GerenciadorTarefas:
             self.atualizar_tarefas_processos()
             self.atualizar_tarefas_memoria()
             self.atualizar_tarefas_global()
-            time.sleep(6)
+            time.sleep(3)
 
 if __name__ == "__main__":
     app = GerenciadorTarefas()
